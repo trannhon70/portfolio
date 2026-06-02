8a29c668d1c606c54b88324a7890bd50de9d6427
trannhon70 <kevintran351996@gmail.com>
2026-06-02 10:53:19 +0700

    deploy

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio",
   images: {
     unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
